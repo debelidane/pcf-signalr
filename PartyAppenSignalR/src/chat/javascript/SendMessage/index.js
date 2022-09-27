f49092f95a52f9b5710280fc27b7e3f1ae0a8e2e
danijel.buljat <danijel.buljat@outlook.com>
2022-09-27 16:03:31 +0000

    add

--- PartyAppenSignalR/src/chat/javascript/SendMessage/index.js
+++ PartyAppenSignalR/src/chat/javascript/SendMessage/index.js
@@ -1,12 +1,21 @@
 module.exports = function (context, req) {
+    let groupName = req.query?.groupName
+
     const message = req.body;
     if (req.headers && req.headers['x-ms-client-principal-name']) {
         message.sender = req.headers['x-ms-client-principal-name'];
     }
-        
-    context.bindings.signalRMessages = [{
+    
+    let signalRMessage = {
         "target": "newMessage",
         "arguments": [ message ]
-    }];
+    }
+
+    if(groupName){
+        signalRMessage.groupName = groupName
+    }
+    
+    context.bindings.signalRMessages = [signalRMessage];
+
     context.done();
 };
